d50a0322133ffd3e24773ab8c3bb9cd5cda4ce29
jennykut12 <jennykut.dj@gmail.com>
2024-01-20 21:53:44 +0100

    update

--- hands-on-projects/bmi/app/layout.tsx
+++ hands-on-projects/bmi/app/layout.tsx
@@ -1,13 +1,26 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+// import '../public/favicon.jpg'
+import 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'BMI Calculator',
   description: 'Calculator your BMI',
-  
+  icons: {
+    icon: [
+      'favicon.ico?v=4',
+    ],
+    apple:[
+      'apple-touch-icon.png?v=4'
+    ],
+    shortcut:[
+      'apple-touch-icon.png'
+    ]
+  },
+  manifest: '/site.webmanifest'
 }
 
 export default function RootLayout({
