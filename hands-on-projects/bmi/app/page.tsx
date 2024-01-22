8ea815b380825588848abf35141c994f423073a4
jennykut12 <jennykut.dj@gmail.com>
2024-01-22 23:23:53 +0100

    update

--- hands-on-projects/bmi/app/page.tsx
+++ hands-on-projects/bmi/app/page.tsx
@@ -3,10 +3,8 @@ import BmiCalc from './components/bmiCalc'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section>
+    <main className="">
         <BmiCalc/>
-      </section>
     </main>
   )
 }
