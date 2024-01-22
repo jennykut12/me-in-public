abf7919290d3b4d8ea5c366b06e1b48b45f716f9
jennykut12 <jennykut.dj@gmail.com>
2024-01-22 22:13:45 +0100

    update

---
+++ hands-on-projects/bmi/app/components/bmiCalc.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function BmiCalc() {
+  return (
+    <div className='bmiCal'>
+      <div className=''>
+        <h1>Body Mass Index Calculator</h1>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default BmiCalc;
