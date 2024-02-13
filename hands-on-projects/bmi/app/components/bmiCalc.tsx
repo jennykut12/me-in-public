ce1995471323e2b7c2916345b0f771fb8065f073
jennykut12 <jennykut.dj@gmail.com>
2024-02-13 23:45:04 +0100

    update

--- hands-on-projects/bmi/app/components/bmiCalc.tsx
+++ hands-on-projects/bmi/app/components/bmiCalc.tsx
@@ -11,6 +11,9 @@ function BmiCalc() {
   const data = [{ name: "Metric" }, { name: "Imperial" }];
   const [tabs, setTabs] = useState(TAB1);
   const [selected, setSelected] = useState(false);
+  const [text, setText] = useState('Enter your height and weight and you will see your BMI result here')
+  const [headerText, setHeaderText] = useState('Hello, Welcome!')
+
   return (
     <div className=" mb-40">
       <div className="bmiCal flex justify-center lg:justify-start lg:px-32 pt-20 lg:pt-0 lg:items-center max-w-full h-[80vh] lg:max-w-[70vw] lg:h-[70vh] sm:rounded-b-[40px] lg:rounded-bl-[0] lg:rounded-br-[40px]">
@@ -28,9 +31,9 @@ function BmiCalc() {
         </div>
       </div>
       <div className=" w-full flex justify-center ">
-        <div className="  shadow-xl w-[90vw] p-8 gap-7 flex flex-col lg:ml-[50vw] -mt-32 lg:-mt-96 lg:w-[40vw] h-64 bg-white rounded-lg absolute z-10">
+        <div className="  shadow-xl w-[90vw] p-8 gap-7 flex flex-col lg:ml-[50vw] -mt-32 lg:-mt-96 lg:max-w-[45vw] h-auto bg-white rounded-lg absolute z-10">
           <p>Enter your details below</p>
-          <div role="tablist" className="flex flex-row space-x-12 w-full">
+          <div role="tablist" className="flex flex-row justify-between w-full">
             {data.map((tab, index) => (
               <a
                 className=" w-96"
@@ -55,20 +58,28 @@ function BmiCalc() {
           </div>
           <div className="flex flex-col w-full">
             {tabs === TAB1 && (
-              <form className=" flex items-center justify-between">
+              <form className="flex flex-col gap-10 justify-center  ">
+                <div className=" flex lg:gap-6 gap-10 justify-between">
                 <div>
                 <label className="text-gray-600">Height</label>
-                <div className=" flex items-center border-2 border-gray-400 w-[35vw] h-12 rounded-xl  lg:w-auto">
-                  <input className=" max-w-40"/>
-                  <p>cm</p>
+                <div className="Input cursor-pointer flex px-4 items-center border-2 focus-within:border-2 focus-within:border-blue border-gray-400 max-w-[35vw] h-12 rounded-xl  lg:max-w-60">
+                  <input type="text" placeholder="0" className="Input bg-transparent max-w-[25vw]"/>
+                  <p className=" text-blue">cm</p>
                 </div>
                 </div>
                 <div>
                 <label className="text-gray-600">Weight</label>
-                <div className=" flex items-center border-2 border-gray-400 w-[35vw] h-12 rounded-xl  lg:w-auto">
-                  <input className="max-w-40"/>
-                  <p>kg</p>
+                <div className="Input focus-within:border-2 focus-within:border-blue flex px-4 items-center border-2 border-gray-400 max-w-[35vw] h-12 rounded-xl  lg:max-w-60">
+                  <input type="text" placeholder="0" className=" Input bg-transparent max-w-[25vw]"/>
+                  <p className=" text-blue">kg</p>
                 </div>
+                </div>
+                </div>
+                <div className="buttonDiv flex px-10 bg-blue w-full h-28 ">
+                  <button className="   ">
+                    <p className=" text-white text-3xl text-start">{headerText}</p>
+                    <p className=" text-white text-start">{text}</p>
+                  </button>
                 </div>
               </form>
             )}
